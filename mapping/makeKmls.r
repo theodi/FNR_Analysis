2463f1a5168839ae2d3bf6262974756b52463adf
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-01 09:24:51 +0100

    Early .kml generation

---
+++ mapping/makeKmls.r
@@ -0,0 +1,29 @@
+library("maptools")
+
+incidents_classes <- structure(c("character", "character", "factor", "factor", "integer", 
+"factor", "integer", "factor", "numeric", "numeric"), .Names = c("DateOfCall", 
+"TimeOfCall", "IncidentGroup", "IncidentStationGround", "FirstPumpArriving_AttendanceTime", 
+"FirstPumpArriving_DeployedFromStation", "SecondPumpArriving_AttendanceTime", 
+"SecondPumpArriving_DeployedFromStation", "latitude", "longitude"
+))
+
+incidents <- read.csv("incidents.csv", header = TRUE, sep = ',', colClasses = incidents_classes, na.strings = 'NULL')
+
+# I keep only the incidents to which the first pump to arrive was the station 
+# ground's and attendance time was withing the target 6 minutes; I presume that
+# the polygon represented by the remaining set of incidents is a good 
+# approximation of the geographical area that can be served by the station in 
+# six minutes. I also drop the columns that are not required in the following.
+incidents <- subset(incidents, (FirstPumpArriving_DeployedFromStation == IncidentStationGround) & (FirstPumpArriving_AttendanceTime <= 360), c('DateOfCall', 'TimeOfCall', 'IncidentStationGround', 'latitude', 'longitude'))
+
+for (station in levels(incidents$IncidentStationGround)) {
+	incidents_station <- subset(incidents, IncidentStationGround == station, c('longitude', 'latitude'))
+	attendance_area_vertices <- chull(incidents_station)
+	attendance_area <- Polygons(list(Polygon(incidents_station[c(attendance_area_vertices, attendance_area_vertices[1]), ])), "1")
+	kmlPolygon(attendance_area, kmlfile = paste(station, ".kml"), name = station, col = "#df0000aa", lwd = 5, border = 4, kmlname = station, kmldescription = "This is <b>only</b> a <a href=’http://www.r-project.org’>R</a> test.")
+}
+
+# for later
+# stations_classes <- structure(c("factor", "character", "numeric", "numeric"), .Names = c("name", "address", "latitude", "longitude"))
+
+# stations <- read.csv("stations.csv", header = TRUE, sep = ',', colClasses = stations_classes, na.strings = 'NULL')
