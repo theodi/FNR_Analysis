118f6ec8983969d85e2242a51765cf34832c2e16
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-03 08:31:01 +0100

    Early .kml creation using buffering
    There still is a issue to solve in the use of the readWKT function with
    a very high number of points as the arguments to LINESTRING

--- mapping/makeKmls.r
+++ mapping/makeKmls.r
@@ -1,4 +1,5 @@
 library("maptools")
+library("rgeos")
 
 incidentsClasses <- structure(c("character", "character", "factor", "factor", "integer", 
 "factor", "integer", "factor", "numeric", "numeric"), .Names = c("DateOfCall", 
@@ -23,8 +24,22 @@ colnames(reachAsSecondPumpArriving) <- c("station", "attendanceTime", "longitude
 reach <- rbind(reachAsFirstPumpArriving, reachAsSecondPumpArriving)
 
 for (s in levels(reach$station)) {
+	sourceStation <- subset(stations, name == s, c('longitude', 'latitude'))[1,]
 	reachStation <- subset(reach, station == s, c('longitude', 'latitude'))
-	reachVertices <- chull(reachStation)
-	reachPolygons <- Polygons(list(Polygon(reachStation[c(reachVertices, reachVertices[1]), ])), "1")
-	kmlPolygon(reachPolygons, kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
+	temp <- reachStation[rep(1:nrow(reachStation), 1, each = 2), ]
+	reachStation[seq(1, nrow(temp) + 1, 2), 1:2] <- sourceStation
+	# TODO: the line below fails unless I heavily constrain the number of 
+	# incidents I create the LINESTRING from. I believe that the problem is a
+	# string too long to be manageable by either geos or R itself. But how
+	# can I create the line otherwise?
+	line = readWKT(paste("LINESTRING(", paste(apply(reachStation[1:21, ], 1, paste, collapse = " "), collapse = ","), ")"))
+	# I have tested experimentally that a 0.0006 buffer width corresponds to
+	# about 100 meters diameter
+	area = gBuffer(line, width = 0.0006, capStyle = "ROUND")
+	kmlPolygon(area@polygons[[1]], kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
 }
+
+#	reachStation <- subset(reach, station == s, c('longitude', 'latitude'))
+#	reachVertices <- chull(reachStation)
+#	reachPolygons <- Polygons(list(Polygon(reachStation[c(reachVertices, reachVertices[1]), ])), "1")
+#	kmlPolygon(reachPolygons, kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
