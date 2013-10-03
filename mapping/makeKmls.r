bbb975eeaf8dd5086213474e5ed94ec9363b555f
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-03 13:32:47 +0100

    Likely final buffer-based algorithm to calculate the <= 6m attendance areas

--- mapping/makeKmls.r
+++ mapping/makeKmls.r
@@ -26,13 +26,17 @@ reach <- rbind(reachAsFirstPumpArriving, reachAsSecondPumpArriving)
 for (s in levels(reach$station)) {
 	sourceStation <- subset(stations, name == s, c('longitude', 'latitude'))[1,]
 	reachStation <- subset(reach, station == s, c('longitude', 'latitude'))
-	temp <- reachStation[rep(1:nrow(reachStation), 1, each = 2), ]
-	temp[seq(1, nrow(temp) + 1, 2), 1:2] <- sourceStation
-	reachStation <- temp
-	line <- Line(reachStation)
-	# I have tested experimentally that a 0.0012 buffer width corresponds to
-	# about 100 meters ray around the original line connecting the station to
-	# the incident, including the thick borders I am currently using
-	area <- gBuffer(SpatialLines(list(Lines(line, ID = s))), width = 0.0012, capStyle = "ROUND")
+	lines <- mapply(function (x, y) { Line(rbind(sourceStation, c(x, y))) }, reachStation$longitude, reachStation$latitude)
+	area <- gBuffer(SpatialLines(list(Lines(lines, ID = s))), width = 0.0012, capStyle = "ROUND")
 	kmlPolygon(area@polygons[[1]], kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
 }
+
+#	temp <- reachStation[rep(1:nrow(reachStation), 1, each = 2), ]
+#	temp[seq(1, nrow(temp) + 1, 2), 1:2] <- sourceStation
+#	reachStation <- temp
+#	line <- Line(reachStation)
+#	# I have tested experimentally that a 0.0012 buffer width corresponds to
+#	# about 100 meters ray around the original line connecting the station to
+#	# the incident, including the thick borders I am currently using
+#	area <- gBuffer(SpatialLines(list(Lines(line, ID = s))), width = 0.0012, capStyle = "ROUND")
+#	kmlPolygon(area@polygons[[1]], kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
