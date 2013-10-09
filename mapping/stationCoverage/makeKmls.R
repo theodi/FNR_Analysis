8522d4c7fccd413cc10ebfa9bf7a637fbdd038d6
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-09 13:43:37 +0100

    Generated a few station coverage .kmls sets for Davetaz to choose what is more appropriate

--- mapping/stationCoverage/makeKmls.R
+++ mapping/stationCoverage/makeKmls.R
@@ -6,30 +6,29 @@ makeKmls.save <- function (preprocessedIncidents = makeKmls.PREPROCESSED_INCIDEN
     library("maptools")
     library("rgeos")
     
-    incidentsClasses <- structure(c("character", "character", "factor", "factor", "integer", "factor", "integer", "factor", "numeric", "numeric"), .Names = c("DateOfCall", "TimeOfCall", "IncidentGroup", "IncidentStationGround", "FirstPumpArriving_AttendanceTime", "FirstPumpArriving_DeployedFromStation", "SecondPumpArriving_AttendanceTime", "SecondPumpArriving_DeployedFromStation", "latitude", "longitude"
-                                    ))
+    incidentsClasses <- structure(c("character", "character", "factor", "factor", "integer", "factor", "integer", "factor", "numeric", "numeric"), .Names = c("DateOfCall", "TimeOfCall", "IncidentGroup", "IncidentStationGround", "FirstPumpArriving_AttendanceTime", "FirstPumpArriving_DeployedFromStation", "SecondPumpArriving_AttendanceTime", "SecondPumpArriving_DeployedFromStation", "latitude", "longitude"))
     incidents <- read.csv(preprocessedIncidents, header = TRUE, sep = ',', colClasses = incidentsClasses, na.strings = 'NULL')
     
     stationsClasses <- structure(c("factor", "character", "numeric", "numeric"), .Names = c("name", "address", "latitude", "longitude"))
     stations <- read.csv(preprocessedStations, header = TRUE, sep = ',', colClasses = stationsClasses, na.strings = 'NULL')
     
-    # I keep only the incidents to which the first pump to arrive was the station 
-    # ground's and attendance time was withing the target 6 minutes; I presume that
-    # the polygon represented by the remaining set of incidents is a good 
-    # approximation of the geographical area that can be served by the station in 
-    # six minutes. I also drop the columns that are not required in the following.
-    reachAsFirstPumpArriving <- subset(incidents, FirstPumpArriving_AttendanceTime <= 360, c('FirstPumpArriving_DeployedFromStation', 'FirstPumpArriving_AttendanceTime', 'longitude', 'latitude'))
-    colnames(reachAsFirstPumpArriving) <- c("station", "attendanceTime", "longitude", "latitude")
-    reachAsSecondPumpArriving <- subset(incidents, SecondPumpArriving_AttendanceTime <= 360, c('SecondPumpArriving_DeployedFromStation', 'FirstPumpArriving_AttendanceTime', 'longitude', 'latitude'))
-    colnames(reachAsSecondPumpArriving) <- c("station", "attendanceTime", "longitude", "latitude")
-    reach <- rbind(reachAsFirstPumpArriving, reachAsSecondPumpArriving)
-    
-    for (s in levels(reach$station)) {
+    # I infer each station's taerget area of coverage by the location of all
+    # the incidents their pump arrives first to, whatever the time it takes
+    for (s in levels(incidents$FirstPumpArriving_DeployedFromStation)) {
         sourceStation <- subset(stations, name == s, c('longitude', 'latitude'))[1,]
-        reachStation <- subset(reach, station == s, c('longitude', 'latitude'))
-        lines <- mapply(function (x, y) { Line(rbind(sourceStation, c(x, y))) }, reachStation$longitude, reachStation$latitude)
-        area <- gBuffer(SpatialLines(list(Lines(lines, ID = s))), width = 0.0012, capStyle = "ROUND")
-        kmlPolygon(area@polygons[[1]], kmlfile = paste("maps/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")
+        coverage <- subset(incidents, (FirstPumpArriving_DeployedFromStation == s) & (FirstPumpArriving_AttendanceTime <= 360), c('longitude', 'latitude'))
+        
+        # the convex hull version
+        coverage_vertices <- chull(coverage)
+        coverage_area <- Polygons(list(Polygon(coverage[c(coverage_vertices, coverage_vertices[1]), ])), "1")
+        
+#         # the buffer version
+#         lines <- mapply(function (x, y) { Line(rbind(sourceStation, c(x, y))) }, coverage$longitude, coverage$latitude)
+#         area <- gBuffer(SpatialLines(list(Lines(lines, ID = s))), width = 0.0012, capStyle = "ROUND")
+#         coverage_area <- area@polygons[[1]]
+        
+        # I create and save the .kml
+        kmlPolygon(coverage_area, kmlfile = paste(folder, "/", s, ".kml", sep = ""), name = s, col = "#df0000aa", lwd = 5, border = 4, kmlname = s, kmldescription = "")        
     }
     
 }
