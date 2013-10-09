953ba37fdded9269bc90cdae6d5b83edb57080da
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-09 21:09:14 +0100

    Wrote getStationResponseTime client-side function to replace same function in 'GetAreaResponseTime.php'

--- web/map prototype/js/data.js
+++ web/map prototype/js/data.js
@@ -5,6 +5,12 @@ var stationsData = undefined;
 var loadData = function (callback) {
 	d3.csv("data/incidents.csv", function (inputData) {
 		incidentsData = inputData;
+		// TODO: is what you see below necessary? why does d3.csv import all columns as strings?
+		_.each(incidentsData, function (incident) {
+			_.each([ 'firstPumpTime', 'secondPumpTime' ], function (columnName) {
+				incident[columnName] = parseFloat(incident[columnName]);
+			});
+		});
 		d3.csv("data/stations.csv", function (inputData) {
 			stationsData = inputData;
 			callback(null);
@@ -25,9 +31,30 @@ var boroughsReload = function (stations) {
 }
 
 
+/* This function replaces the 'getStationResponseTime' function in the 
+   'GetAreaResponseTime.php' file. It returns the average first pump 
+   attendance time to incidents located in the 'station' area, by all 
+   stations excluding the ones listed in closedStations */ 
+var getStationResponseTime = function (station, closedStations) {
+	var temp = _.map(_.filter(incidentsData, function (incident) {
+		return (incident.station == station) && !_.contains(closedStations, incident.firstPumpStation);
+	}), function (incident) { return incident.firstPumpTime; });
+	return _.reduce(temp, function (memo, num) { return memo + num; }, 0) / temp.length;
+};
+
+
 /* This function replaces the calls to GetAreaResponseTime.php when a borough is
    specified. */
 var getBoroughResponseTime = function (borough, closedStations) {
+
+	var getStationsInBorough = function (borough) {
+		return _.map(_.filter(stationsData, function (r) {
+			return r.borough == borough;
+		}), function (r) { return r.borough; });
+	};
+
 	closedStations = [ ].concat(closedStations);
-	
+	_.each(getStationsInBorough(borough, function (station) {
+
+	}));
 }
