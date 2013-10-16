069db5bd27ae933ce49fcdc47efc8a0e19797645
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-16 12:17:07 +0100

    Changed the borough preprocessing script to calculate the default response time, updated all json files

--- preprocessing/boroughs/preprocess.js
+++ preprocessing/boroughs/preprocess.js
@@ -1,37 +1,54 @@
 var fs = require('fs'),
     _ = require('underscore'),
+    csv = require('csv'), // http://www.adaltas.com/projects/node-csv/
     argv = require('optimist') // https://github.com/substack/node-optimist
-      .usage('Usage: $0 --in [filename] --out [folder]')
-        .demand([ 'in', 'out' ])
+      .usage('Usage: $0 --kml [filename] --incidents [incidents .csv folder] --out [folder]')
+        .demand([ 'kml', 'incidents', 'out' ])
         .alias('in', 'i')
         .alias('out', 'o')
         .argv, 
     parseString = require('xml2js').parseString;
 
-var result = fs.readFileSync(argv.in);
+var mean = function (a) {
+    a = [ ].concat(a);
+    return _.reduce(a, function (memo, num) { return memo + num; }, 0.0) / a.length;
+}
+
+var result = fs.readFileSync(argv.kml);
 parseString(result, function (err, result) {
     var id = 0;
     _.each(result.kml.Document[0].Placemark, function (d) {
-        var polygon = _.map(d.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].split(" "), function (point) {
-            return [ parseFloat(point.split(",")[0]), parseFloat(point.split(",")[1]) ];
-        });
-        var borough = { type: "FeatureCollection" };
-        borough.features = [ ];
-        borough.features.push({
-            type: "Feature",
-            id: ++id,
-            properties: {
-                "borough": d.name[0],
-                // TODO: Davetaz wrote the borough's default response time here
-                // but I am not sure it is the right thing to do
-                "response": 0,
-            },
-            geometry: {
-                type: "Polygon",
-                coordinates: [ polygon ],
-            }
-        });
-        fs.writeFileSync(argv.out + "/" + d.name + ".json", JSON.stringify(borough));
-
+        var performance = [ ];
+        csv()
+            .from.stream(fs.createReadStream(__dirname + '/' + argv.incidents + '/' + d.name[0] + '.csv'), {
+                columns: true
+            })
+            .on('record', function (row, index) {
+                performance.push(parseFloat(row.firstPumpTime));
+            })
+            .on('error', function (error) {
+              console.log(error.message);
+            })
+            .on('end', function (count) {
+                performance = mean(performance);
+                var polygon = _.map(d.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].split(" "), function (point) {
+                    return [ parseFloat(point.split(",")[0]), parseFloat(point.split(",")[1]) ];
+                });
+                var borough = { type: "FeatureCollection" };
+                borough.features = [ ];
+                borough.features.push({
+                    type: "Feature",
+                    id: ++id,
+                    properties: {
+                        "borough": d.name[0],
+                        "response": performance,
+                    },
+                    geometry: {
+                        type: "Polygon",
+                        coordinates: [ polygon ],
+                    }
+                });
+                fs.writeFileSync(argv.out + "/" + d.name + ".json", JSON.stringify(borough));
+            });
     });
 });
