64fbce24b88edc1ab19edb3e1e87124adb7da5b4
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-20 12:49:46 +0100

    Many improvements
    - Incredibly faster footfall.preprocess.consolidate thanks to data.table
    - Recovered the footfall.preprocess.readMay2013 function
    - renamed the references to davetaz to "simplified" (e.g. simplified
    latitude)

--- preprocessing/footfall/preprocess.R
+++ preprocessing/footfall/preprocess.R
@@ -8,20 +8,20 @@ outputAreas.preprocess.save <- function (outputAreas, filename = "outputAreas.cs
 
 outputAreas.preprocess.readAndClean <- function (relevantGridIDs) {
 
-    # Below is the size in latitude and longitude of the map square Davetaz
-    # has experimentally identified as relevant for visualisation
-    DAVETAZ_SQUARE_LATITUDE_SIZE <- 0.001
-    DAVETAZ_SQUARE_LONGITUDE_SIZE <- 0.0015
+    # Below is the size in latitude and longitude of the simplified map squares
+    # we experimentally identified as relevant for visualisation
+    SIMPLIFIED_SQUARE_LATITUDE_SIZE <- 0.001
+    SIMPLIFIED_SQUARE_LONGITUDE_SIZE <- 0.0015
 
-	# The average latitude of the output areas for London is 51.52447 degrees. I use
-	# that to approximate the length of a degree of latitude and longitude across 
-	# the whole of London. The result is below. The calculation is courtesy of 
-	# http://www.csgnetwork.com/degreelenllavcalc.html 
+	# The average latitude of the output areas for London is 51.52447 degrees. 
+	# We use that to approximate the length of a degree of latitude and 
+	# longitude across the whole of London. The result is below. The calculation 
+	# is courtesy of http://www.csgnetwork.com/degreelenllavcalc.html 
 	# TODO: double-check results with alternative source
 	LENGTH_OF_A_DEGREE_OF_LATITUDE <- 111.25826132219737 # km
 	LENGTH_OF_A_DEGREE_OF_LONGITUDE <- 69.4032968251825 # km
 
-	outputAreas <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/telefonica-output-areas.csv.gz", sep = ""))
+	outputAreas <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/output-areas.csv.gz", sep = ""))
 	colnames(outputAreas) <- c("telefonicaGridId", "longitudeCentre", "latitudeCentre", "areaSqKm")
 
 	# I remove from the areas the rows referring to grid ids that are not listed
@@ -30,9 +30,9 @@ outputAreas.preprocess.readAndClean <- function (relevantGridIDs) {
 
 	# I replace longitude and latitude with the simplified grid we use for 
 	# this exercise
-    outputAreas[, c("davetazLatitude", "davetazLongitude") ] <- cbind(
-	    outputAreas$latitude %/% DAVETAZ_SQUARE_LATITUDE_SIZE * DAVETAZ_SQUARE_LATITUDE_SIZE,
-	    outputAreas$longitude %/% DAVETAZ_SQUARE_LONGITUDE_SIZE * DAVETAZ_SQUARE_LONGITUDE_SIZE
+    outputAreas[, c("simplifiedLatitude", "simplifiedLongitude") ] <- cbind(
+	    outputAreas$latitude %/% SIMPLIFIED_SQUARE_LATITUDE_SIZE * SIMPLIFIED_SQUARE_LATITUDE_SIZE,
+	    outputAreas$longitude %/% SIMPLIFIED_SQUARE_LONGITUDE_SIZE * SIMPLIFIED_SQUARE_LONGITUDE_SIZE
     )    
     outputAreas <- outputAreas[, !(names(outputAreas) %in% c("latitude", "longitude"))]
 
@@ -47,58 +47,54 @@ outputAreas.preprocess.readAndClean <- function (relevantGridIDs) {
 	# TODO: the calculation below is not completely correct because of the 
 	# Earth's curvature, but as we need this to calculate an 'index' rather than
 	# the actual area, we can tolerate the issue
-    outputAreas$area <- outputAreas$width * outputAreas$heigth / DAVETAZ_SQUARE_LONGITUDE_SIZE /DAVETAZ_SQUARE_LATITUDE_SIZE
+    outputAreas$area <- outputAreas$width * outputAreas$heigth / SIMPLIFIED_SQUARE_LONGITUDE_SIZE /SIMPLIFIED_SQUARE_LATITUDE_SIZE
 
 	# I force the class of the columns and fix the formats if necessary
 	outputAreas$telefonicaGridId <- as.factor(outputAreas$telefonicaGridId)
-	outputAreas[, names(outputAreas) %in% c("telefonicaGridId", "davetazLongitude", "davetazLatitude", "area")]
+	outputAreas[, names(outputAreas) %in% c("telefonicaGridId", "simplifiedLongitude", "simplifiedLatitude", "area")]
 }
 
 
-footfall.preprocess.readDec2012 <- function (filenames = c("footfall-London-09-dec-2012-15-dec-2012.csv.gz")) {
+footfall.preprocess.readDec2012 <- function (filenames = c("footfall-09-dec-2012-15-dec-2012.csv.gz")) {
 		data = data.frame()
-		for (filename in filenames) {
-			temp <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/", filename, sep = ""))
-			temp <- temp[, c('Date', 'Time', 'Grid_ID', 'Total')]
-			colnames(temp) <- c("date", "time", "telefonicaGridId", "footfall")
-			# remove any duplicates
-			temp <- temp[!duplicated(paste(temp$telefonicaGridId, temp$date, temp$time, temp$footfall)),]
-			# I force the class of the columns and fix the formats if necessary
-			temp$telefonicaGridId <- as.factor(temp$telefonicaGridId)
-			# December's dates need being converted to R's format
-			temp$date <- as.Date(temp$date, "%d/%m/%Y")
-			temp$time <- as.factor(as.numeric(sapply(temp$time, function (t) { unlist(strsplit(as.character(t), ":"))[1] })))
-			temp$day <- as.factor(weekdays(as.Date(temp$date)))
-			temp$footfall <- as.numeric(temp$footfall)
-			temp <- temp[, c("telefonicaGridId", "day", "time", "footfall")]
-			data <- rbind(data, temp)
-		}
-		data
-		# TODO: why is footfall character when the data frame is returned by this function?!?!? 
+	for (filename in filenames) {
+		temp <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/", filename, sep = ""))
+		temp <- temp[, c('Date', 'Time', 'Grid_ID', 'Total')]
+		colnames(temp) <- c("date", "time", "telefonicaGridId", "footfall")
+		# remove any duplicates
+		temp <- temp[!duplicated(paste(temp$telefonicaGridId, temp$date, temp$time, temp$footfall)),]
+		# I force the class of the columns and fix the formats if necessary
+		temp$telefonicaGridId <- as.factor(temp$telefonicaGridId)
+		# December's dates need being converted to R's format
+		temp$date <- as.Date(temp$date, "%d/%m/%Y")
+		temp$time <- as.factor(as.numeric(sapply(temp$time, function (t) { unlist(strsplit(as.character(t), ":"))[1] })))
+		temp$day <- as.factor(weekdays(as.Date(temp$date)))
+		temp$footfall <- as.numeric(temp$footfall)
+		temp <- temp[, c("telefonicaGridId", "day", "time", "footfall")]
+		data <- rbind(data, temp)
+	}
+	data
 }
 
 
-# TODO: this has not been maintanied for a while
-footfall.preprocess.readMay2013 <- function (filenames = c("footfall-UK-13-may-2013-19-may-2013.csv.gz")) {
+footfall.preprocess.readMay2013 <- function (filenames = c("footfall-13-may-2013-19-may-2013.csv.gz")) {
 		data = data.frame()
-		for (filename in filenames) {
-			temp <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/", filename, sep = ""))			
-			temp <- temp[, c('Date', 'Time', 'Grid.ID', 'Total')]
-			colnames(temp) <- c("date", "time", "telefonicaGridId", "footfall")
-			# remove any duplicates
-			temp <- temp[!duplicated(paste(temp$telefonicaGridId, temp$date, temp$time, temp$footfall)),]
-			# I force the class of the columns and fix the formats if necessary
-			temp$telefonicaGridId <- as.factor(temp$telefonicaGridId)
-			temp$date <- as.Date(temp$date)
-			temp$time <- as.factor(as.numeric(sapply(temp$time, function (t) { unlist(strsplit(as.character(t), ":"))[1] })))
-			temp$time <- as.factor(temp$time)
-			temp$day <- weekdays(as.Date(temp$date))
-			temp$day <- as.factor(temp$day)
-			temp$footfall <- as.numeric(temp$footfall)
-			temp <- temp[, c("telefonicaGridId", "day", "time", "footfall")]
-			data <- rbind(data, temp)
-		}
-		data
+	for (filename in filenames) {
+		temp <- read.csv(paste(footfall.preprocess.TELEFONICA_REFERENCE_DATA_FOLDER, "/", filename, sep = ""))	
+		temp <- temp[, c('Date', 'Time', 'Grid.ID', 'Total')]
+		colnames(temp) <- c("date", "time", "telefonicaGridId", "footfall")
+		# remove any duplicates
+		temp <- temp[!duplicated(paste(temp$telefonicaGridId, temp$date, temp$time, temp$footfall)),]
+		# I force the class of the columns and fix the formats if necessary
+		temp$telefonicaGridId <- as.factor(temp$telefonicaGridId)
+		temp$date <- as.Date(temp$date)
+		temp$time <- as.factor(as.numeric(sapply(temp$time, function (t) { unlist(strsplit(as.character(t), ":"))[1] })))
+		temp$day <- as.factor(weekdays(as.Date(temp$date)))
+		temp$footfall <- as.numeric(temp$footfall)
+		temp <- temp[, c("telefonicaGridId", "day", "time", "footfall")]
+		data <- rbind(data, temp)
+	}
+	data
 }
 
 
@@ -116,23 +112,13 @@ footfall.preprocess.fixMissingFootfall <- function (footfall) {
 }
 
 
-# Produces one line per grid per day per hour from non-summarised (but 
-# clean) footfall data, typically data sets from more than one week 
-footfall.preprocess.consolidate <- function (rawFootfall) {
-	library(plyr)
-	# if I don't convert the keys to character, ddply fails inexplicably
-	rawFootfall$telefonicaGridId <- as.character(rawFootfall$telefonicaGridId)
-	rawFootfall$day <- as.character(rawFootfall$day)
-	rawFootfall$time <- as.character(rawFootfall$time)
-	# Note that I can't run ddply on the entire dataset, as R crashes with a
-	# segmentation fault!
-	results <- data.frame()
-	for (tgi in unique(rawFootfall$telefonicaGridId)) {
-		print(paste("Doing grid id", tgi, "..."))
-		areaFootfall <- subset(rawFootfall, telefonicaGridId == tgi)
-		results <- rbind(results, ddply(areaFootfall, .(telefonicaGridId, day, time), summarise, footfall = mean(footfall)))
-	}
-	results
+# Produces one line per Telefonica grid id per day per hour from 
+# footfall data replacing each sample's footfall with their average, typically
+# fro data sets that cover more weeks 
+footfall.preprocess.consolidate <- function (footfall) {
+	library(data.table)
+	footfall <- data.table(footfall)
+	footfall[, list(footfall = round(mean(footfall), 0)), by = 'telefonicaGridId,day,time']
 }
 
 
