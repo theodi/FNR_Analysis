d8fdd4c0290f2313b798777bed4e56da7e89280b
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-10-24 07:21:20 +0100

    Updated borough defs with new default footfall score

--- preprocessing/boroughs/boroughsPreprocess.js
+++ preprocessing/boroughs/boroughsPreprocess.js
@@ -9,31 +9,50 @@ var fs = require('fs'),
         .argv, 
     parseString = require('xml2js').parseString;
 
-var mean = function (a) {
-    a = [ ].concat(a);
-    return _.reduce(a, function (memo, num) { return memo + num; }, 0.0) / a.length;
+var mean = function (values) {
+    return _.reduce(values, function (memo, num) { return memo + num; }, 0.0) / values.length;
 }
+
+
+var median = function (values) {
+    // Thanks to http://caseyjustus.com/finding-the-median-of-an-array-with-javascript
+    values.sort(function(a,b) {return a - b;});
+    var half = Math.floor(values.length / 2);
+    return (values.length % 2 == 0) ? values[half] : (values[half - 1] + values[half]) / 2.0;
+}
+
+// TODO
+// This is a simplified version of getBoroughScoreM of 'data.js', this function
+// should be manually kept in line with the other, until a better solution is
+// implemented.
+var getBoroughScore = function (responseTimesSeries, footfallSeries) {
+    var A = 0.75,
+        medianResponseTimes = median(_.map(responseTimesSeries, function (x) { return x / 60; })),
+        medianFootfall = median(footfallSeries);
+    return Math.pow(medianResponseTimes, A) + 
+        Math.pow(Math.log(medianFootfall) / Math.log(10), 1 - A);
+};
 
 var result = fs.readFileSync(argv.kml);
 parseString(result, function (err, result) {
     var id = 0;
     _.each(result.kml.Document[0].Placemark, function (d) {
         var performance = [ ];
-        var score = [ ];
+        var footfall = [ ];
         csv()
             .from.stream(fs.createReadStream(__dirname + '/' + argv.incidents + '/' + d.name[0] + '.csv'), {
                 columns: true
             })
             .on('record', function (row, index) {
                 performance.push(parseFloat(row.firstPumpTime));
-                score.push(parseFloat(row.score));
+                footfall.push(parseFloat(row.footfall));
             })
             .on('error', function (error) {
               console.log(error.message);
             })
             .on('end', function (count) {
+                score = parseFloat(getBoroughScore(performance, footfall));
                 performance = Math.round(mean(performance));
-                score = parseFloat(mean(score).toFixed(2));
                 var polygon = _.map(d.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].split(" "), function (point) {
                     return [ parseFloat(point.split(",")[0]), parseFloat(point.split(",")[1]) ];
                 });
